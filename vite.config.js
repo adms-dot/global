073da77d59af7d35f7eee6fcff5a5af5192d61d5
ADMS <accessadms@gmail.com>
2024-11-19 09:32:03 +0530

    initial

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/global/', // Replace with the name of your GitHub repo
+  base: '/globalWellness/', // Replace with the name of your GitHub repo
 })
