1f798fe628bf9f61c1b19c799372a8e25e2d637d
ADMS <accessadms@gmail.com>
2024-11-28 10:34:21 +0530

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import hero from "../assets/about-hero.png"
 import image1 from "../assets/about1.png"
+import image2 from "../assets/about2.png"
+import image3 from "../assets/about3.png"
 
 
 function About() {
@@ -12,11 +14,11 @@ function About() {
         <h3 className='text-center text-xl lg:text-2xl text-zinc-700 pt-4'>A Team dedicated to your Child's Happiness</h3>
         <img className='pt-6 lg:pt-10 '  src={hero} alt="Team_image" />
 
-        <div className='flex pt-10 justify-around items-center  w-full '>
+        <div className='flex pt-10 justify-evenly items-center  w-full '>
           <img className="h-32 lg:h-96 " src={image1} alt="" />
-          <div className='h-32 border py-2 px-4 border-zinc-300 lg:h-80 rounded-lg w-80 lg:w-2/3 bg-zinc-100'>
-          <h1>Vision & Mission</h1>
-          <p className='text-[8px] text-justify'>Dr. Priyanka Kalra, a distinguished Consultant Psychiatrist and a member of the Indian Association of Child and Adolescent Psychiatry, founded a center with a clear vision to provide a dedicated platform for children, adolescents, and parents. The mission is to reach out to every child who needs help, offering a sanctuary for consultations on various issues or challenges faced in daily life.</p>
+          <div className='h-32 border py-2 px-4 lg:py-4 lg:px-6 border-zinc-300 lg:h-80 rounded-lg w-80 lg:w-1/2 bg-zinc-100'>
+          <h1 className='font-semibold lg:text-4xl'>Vision & Mission</h1>
+          <p className='text-[8px] lg:text-xl lg:pt-4 text-zinc-700 text-justify'>Dr. Priyanka Kalra, a distinguished Consultant Psychiatrist and a member of the Indian Association of Child and Adolescent Psychiatry, founded a center with a clear vision to provide a dedicated platform for children, adolescents, and parents. The mission is to reach out to every child who needs help, offering a sanctuary for consultations on various issues or challenges faced in daily life.</p>
           
           
           </div>
